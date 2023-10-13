5d4118268e71763b21e8297ae5fb713bb79057bc
DainSlash <powerdick0@gmail.com>
2023-10-13 01:44:27 -0300

    Consultar telefone funcionando

--- Telefones.c
+++ Telefones.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include <locale.h>
-
+#include <string.h>
 
 //escopo de funcoes
 void limparScanf();
 void cadastrarTelefone();
-
+void consultarTelefone();
 
 int main()
 {
     //MENU
+    FILE *telefonesTXT = fopen("Telefones.txt", "a");//Cria o arquivo telefones caso não exista.
+    fclose(telefonesTXT);
     int o=0;
     while(o!=5){
-        printf("\n\nMenu de Opcoes\n"
+        printf("Menu de Opcoes\n"
                 "\n1 - Cadastrar Telefone"
                 "\n2 - Consultar Telefone"
                 "\n3 - Alterar Telefone"
@@ -55,18 +56,54 @@ void limparScanf(){
 void cadastrarTelefone(){
     FILE *telefonesTXT = fopen("Telefones.txt", "a");
     char NOME[50], TELEFONE[13];
-    int contador=0;
+    char vazio=0;
     printf("\nCadastrando dados\n\n");
     do{
-        if(contador>0) printf("“\nNome ou telefone em branco, digite novamente\n");
+        if(vazio==1) printf("“\nNome ou telefone em branco, digite novamente\n"); //depois de rodar novamente o loop, sinaliza o que o nome ou o telefone estavam vazios
         printf("Nome: ");
-        scanf(" %49[^\n]",NOME); //tamanho de 19, ja que eh necessario um espaco para /0
+        scanf(" %49[^\n]",NOME);
         printf("Telefone: ");
         scanf(" %12[^\n],",TELEFONE);
-        contador++;
-    }while(NOME[0] == NULL || TELEFONE[0] == NULL);
+        vazio=1; 
+    }while(NOME[0] == 0 || TELEFONE[0] == 0); //se o primeiro valor de nome ou telefone forem vazios, roda o loop de novo.
     fprintf(telefonesTXT,"%s,%s\n",NOME,TELEFONE);
     fclose(telefonesTXT);
-    printf("\nDados cadastrados\n");
-    
+    printf("\nDados cadastrados!\n%s %s\n\n\n",NOME,TELEFONE);
+}
+
+void consultarTelefone(){
+    FILE *telefonesTXT = fopen("Telefones.txt", "r");
+    char ENTRADA[50], NOME[50], TELEFONE[13], ch = 0;
+    int tamanho;
+    printf("\n\nConsultando dados\n");
+    printf("\nNome: ");
+    scanf(" %49[^\n]",ENTRADA);
+    NOME[0] = 0;
+    while (1){
+        ch = getc(telefonesTXT);
+        tamanho = strlen(NOME);
+        if(ch==10){ //se ch for new line, então recomeça o vetor nome, 
+            tamanho = 0;
+            ch = getc(telefonesTXT); //vai para o proximo ch, para tirar o nw de ch.
+        }
+        NOME[tamanho] = ch;
+        NOME[tamanho+1] = 0;
+        if(strcmp(NOME, ENTRADA) == 0)break;//se o nome de entrada bater com algum nome do arquivo quebra o loop
+        if(ch==EOF){
+            printf("\nTelefone de %s nao cadastrado\n\n\n",ENTRADA);
+            return;
+        };
+        tamanho = (tamanho==49) ? 0: tamanho;
+    }
+    ch = getc(telefonesTXT); //pega a ,
+    tamanho=0;
+    TELEFONE[0] = 0;
+    while (tamanho<11){
+        ch =getc(telefonesTXT);
+        tamanho = strlen(TELEFONE);
+        TELEFONE[tamanho] = ch;
+        TELEFONE[tamanho+1] = 0; //pega o numero
+    }
+    printf("\nTelefone encontrado com sucesso, %s %s.\n\n\n",NOME,TELEFONE);
+    fclose(telefonesTXT);
 }
